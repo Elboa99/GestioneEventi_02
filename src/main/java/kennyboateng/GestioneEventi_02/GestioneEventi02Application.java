9477b4731926f836f0d0b333ec952d3b7e6ccf66
Kenny Boateng <kennyboateng.99@gmail.com>
2024-09-20 13:18:32 +0200

    Initial commit

---
+++ src/main/java/kennyboateng/GestioneEventi_02/GestioneEventi02Application.java
@@ -0,0 +1,13 @@
+package kennyboateng.GestioneEventi_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventi02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventi02Application.class, args);
+	}
+
+}
